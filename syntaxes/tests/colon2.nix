aa8a59d845efa5bbe69713dfddadd345611e632b
Noortheen Raja <jnoortheen@gmail.com>
2025-09-29 12:33:33 +0530

    fix(syntax): syntax highlighting issue with colon after interpolation in nested attrset
    fixes #355

---
+++ syntaxes/tests/colon2.nix
@@ -0,0 +1,22 @@
+nps.stacks.monitoring.prometheus.config = lib.mkIf cfg.enablePrometheusExport {
+  scrape_configs = [
+    {
+      job_name = "blocky";
+      honor_timestamps = true;
+      metrics_path = "/metrics";
+      scheme = "http";
+      static_configs = [{targets = ["${name}:6000"];}];
+    }
+  ];
+};
+
+nps.stacks.${name} = {
+  settings = {
+    prometheus = {
+      enabled = cfg.enablePrometheusExport;
+      level = "aggregated";
+      listen_address = "0.0.0.0";
+      listen_port = 6060;
+    };
+  };
+};
