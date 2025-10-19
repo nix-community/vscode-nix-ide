b7b462efca4c65148aad5dbeb9f9413c64826705
Noortheen Raja <jnoortheen@gmail.com>
2025-10-20 05:09:52 +0530

    test: small test update

--- syntaxes/tests/sample.nix
+++ syntaxes/tests/sample.nix
@@ -4,6 +4,7 @@ let
   a = abort "will never happen";
   b = "hello";
   c = "world";
+  # the following lines miss semi-colons on purpose
   path = ./relative/path
   sp_path = ./relative/${path}
 in b + c
