df3c02fd30d57f47f854b6c25ed765819d697585
Noortheen Raja <jnoortheen@gmail.com>
2025-10-30 21:11:16 +0530

    style: fix syntax

--- syntax-tests/colon2.nix
+++ syntax-tests/colon2.nix
@@ -1,22 +1,26 @@
-nps.stacks.monitoring.prometheus.config = lib.mkIf cfg.enablePrometheusExport {
-  scrape_configs = [
-    {
-      job_name = "blocky";
-      honor_timestamps = true;
-      metrics_path = "/metrics";
-      scheme = "http";
-      static_configs = [{targets = ["${name}:6000"];}];
-    }
-  ];
-};
-
-nps.stacks.${name} = {
-  settings = {
-    prometheus = {
-      enabled = cfg.enablePrometheusExport;
-      level = "aggregated";
-      listen_address = "0.0.0.0";
-      listen_port = 6060;
+{
+  nps.stacks.monitoring.prometheus.config = lib.mkIf cfg.enablePrometheusExport {
+    scrape_configs = [
+      {
+        job_name = "blocky";
+        honor_timestamps = true;
+        metrics_path = "/metrics";
+        scheme = "http";
+        static_configs = [ { targets = [ "${name}:6000" ]; } ];
+        shell = /* sh */ ''
+          echo "Starting Prometheus scrape for Blocky at ${name}:6000"
+        '';
+      }
+    ];
+  };
+  nps.stacks.${name} = {
+    settings = {
+      prometheus = {
+        enabled = cfg.enablePrometheusExport;
+        level = "aggregated";
+        listen_address = "0.0.0.0";
+        listen_port = 6060;
+      };
     };
   };
-};
+}
